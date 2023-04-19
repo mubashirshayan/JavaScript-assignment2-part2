c7a4fca2e6bf819b8d8d309f9862dcf404f20741
mubashirshayan <121729482+mubashirshayan@users.noreply.github.com>
2023-04-19 20:29:34 +0500

    New files

---
+++ app.js
@@ -0,0 +1,34 @@
+//JavaScript Assignment No2&Ch2
+//Question no1
+//var username;
+//Question no2
+//var myName='Mubashir Shayan'
+//Question no3
+//var message;
+//(b)
+//var message='Hello World'
+//(c)
+//alert(message);
+//Question no4
+/*var name='Mubashir shayan'
+var age='19 years old'
+var skill='Certified Web Application Development'
+alert(name);
+alert(age)
+alert(skill)*/
+//Question no5
+/*var str='Pizza pizz piz'
+alert(str)*/
+//Question no6
+/*var email='My email address is'+'shayanraees890@gmail.com'
+alert(email);*/
+//Question no7
+/*var book='I ma trying to learn from A smarter way to learn JavaScript '
+alert(book);*/
+//Question no8
+/*var str='Yeh! I can write Html content through JavaScript'
+document.write(str);*/
+//Question no9
+/*var doc='▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬'
+document.write(doc);*/
+
